69afb85eaa19c1602e7d3a46739b3044f102e465
Austin Schrader <austin.jschrader@gmail.com>
2021-11-09 22:36:42 -0800

    Add a validation util for the signup form

---
+++ util/validators.js
@@ -0,0 +1,30 @@
+module.exports.validateRegisterInput = (
+  userName,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (userName.trim() === "") {
+    errors.userName = "Username must not be empty";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty";
+  } else {
+    const regEx =
+      /^([0-9a-zA-z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regex)) {
+      errors.email = "Email must be a valid email address";
+    }
+  }
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0,
+  };
+};
