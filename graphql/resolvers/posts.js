caeec357cfaf2f3597f78b341af414dd8a34c3a2
Austin Schrader <austin.jschrader@gmail.com>
2021-11-14 20:59:47 -0800

    Add functionality to sort posts in desc order

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -5,7 +5,7 @@ module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -14,7 +14,7 @@ module.exports = {
     async getPost(_, { postId }) {
       try {
         const post = await Post.findById(postId);
-        
+
         if (post) {
           return post;
         } else {
